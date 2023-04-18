f4fff13332eea78a87c325bf7441ec3aede9618c
Ilya <felagunt@live.ru>
2023-04-18 22:57:30 +0300

    rubish alarm manager

--- app/src/main/java/com/example/alarmapp/alarmManager_feature/AlarmReceiver.kt
+++ app/src/main/java/com/example/alarmapp/alarmManager_feature/AlarmReceiver.kt
@@ -3,10 +3,18 @@ package com.example.alarmapp.alarmManager_feature
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.example.alarmapp.alarm_feature.domain.repository.AlarmRepository
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class AlarmReceiver: BroadcastReceiver() {
 
+    @Inject
+    lateinit var repository: AlarmRepository//here we'll use user fields as a repos etc
+
     override fun onReceive(context: Context?, intent: Intent?) {
-        val message = intent?.getStringExtra("Extra_message")
+        val message = intent?.getStringExtra("Extra_message") ?: return
+        println("Alarm triggered: $message")
     }
 }
--- app/src/main/java/com/example/alarmapp/alarmManager_feature/data/AndroidAlarmScheduler.kt
+++ app/src/main/java/com/example/alarmapp/alarmManager_feature/data/AndroidAlarmScheduler.kt
@@ -1,6 +1,8 @@
 package com.example.alarmapp.alarmManager_feature.data
 
+import android.annotation.SuppressLint
 import android.app.AlarmManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import com.example.alarmapp.alarmManager_feature.AlarmItem
@@ -14,17 +16,31 @@ class AndroidAlarmScheduler(
 
     private val alarmManager = context.getSystemService(AlarmManager::class.java)
 
+    @SuppressLint("MissingPermission")
     override fun scheduler(item: AlarmItem) {
         val intent = Intent(context, AlarmReceiver::class.java).apply {
             putExtra("Extra_message", item.message)//data send to
         }
         alarmManager.setExactAndAllowWhileIdle(
             AlarmManager.RTC_WAKEUP,
-            item.time.atZone(ZoneId.systemDefault()).toEpochSecond() * 1000
+            item.time.atZone(ZoneId.systemDefault()).toEpochSecond() * 1000,
+            PendingIntent.getBroadcast(
+                context,
+                item.hashCode(),//item.id.hashCode()
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
         )
     }
 
     override fun cancel(item: AlarmItem) {
-        TODO("Not yet implemented")
+        alarmManager.cancel(
+            PendingIntent.getBroadcast(
+                context,
+                item.hashCode(),
+                Intent(context, AlarmReceiver::class.java),
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+        )
     }
 }
